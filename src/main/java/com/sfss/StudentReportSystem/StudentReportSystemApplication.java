42506b52bebd511e3b4cf472dfcdaa8bad7b258b
sam <salmanpatel453@gmail.com>
2024-11-01 14:43:18 -0400

    done

---
+++ src/main/java/com/sfss/StudentReportSystem/StudentReportSystemApplication.java
@@ -0,0 +1,16 @@
+package com.sfss.StudentReportSystem;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentReportSystemApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(StudentReportSystemApplication.class, args);
+		System.out.println("hellow World");
+	}
+
+}
